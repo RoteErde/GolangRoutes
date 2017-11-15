bdc2191e79bcb19dab6b97feb36f733adc18b878
roteErde <not_so_good@outlook.com>
2017-11-15 13:37:53 +0800

    restructure folder

--- src/routes.go
+++ src/routes.go
@@ -1,16 +1,16 @@
 package main
 
 import "net/http"
+import "fmt"
 func getPerson(id string) string{
 	url := "HTTPS://myinfo.api.gov.sg/dev/L0/v1/person/" + id + "/"
 	resp, err:= http.Get(url)
 
 	if resp.Status == "200"{
-
-	}else {
+		fmt.Print("ok")
+	}else{
 		return err.Error()
 	}
 
-
-
+	return ""
 }
